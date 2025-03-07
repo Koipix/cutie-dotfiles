203b58c1b4e74c6b400c621977c135204399985b
koipix <koi.devt@gmail.com>
2025-03-07 19:34:39 +0800

    Updated script
    Now backups user's current config before replacing it

--- script/mklink.sh
+++ script/mklink.sh
@@ -2,41 +2,41 @@
 
 DOTFILES_DIR=~/dotfiles
 CONFIG_DIR=~/.config
+BACKUP_DIR=~/dotfiles_backup
+
+mkdir -p "$BACKUP_DIR"
+mkdir -p "$DOTFILES_DIR"
 
 # awway of dotfiles; feew fwee to add youw dotfiles hewe with the wight souwce path
 declare -A DOTFILES
+
 DOTFILES=(
     [neofetch]="$CONFIG_DIR/neofetch"
-    [kvantum]="$CONFIG_DIR/Kvantum"
+    [Kvantum]="$CONFIG_DIR/Kvantum"
 )
-
-# check if dotfile diwectowy exist
-mkdir -p "$DOTFILES_DIR"
 
 for key in "${!DOTFILES[@]}"; do
     TARGET="${DOTFILES[$key]}"
-    SOURCE="$DOTFILES_DIR/$(basename "$TARGET")"
+    SOURCE="$DOTFILES_DIR/$key"
 
-    echo "DOTFILE: $key"
-    echo "FROM: $TARGET"
-    echo "TO: $SOURCE"
-
-    # check if dotfile alweady exist
+    # check if dotfile alweady exist othewise cweate the folder
     mkdir -p "$(dirname "$SOURCE")"
 
-    # skip loop if alweady symlinked (pwevent bweaking my systuwm)
+    # skip woop if alweady symlinked (pwevent bweaking my systuwm)
     if [ -L "$TARGET" ]; then
-        echo "$key is already symlinked uwu"
-        continue
+        echo "Replacing your config for $key"
+        rm "$TARGET"
     fi
 
-    # move existing file if it's not symlinked together because they were meant to be together <3
+    # backup existing config if it's not symlinked, to ensure safe delivery~
     if [ -e "$TARGET" ] && [ ! -L "$TARGET" ]; then
-        mv "$TARGET" "$SOURCE"
+        mv "$TARGET" "$BACKUP_DIR/"
+        echo "Backed up $key to $BACKUP_DIR"
     fi
 
-    # # cweate symwink
+    # cweate symwink because they were meant to be together <3
     ln -s "$SOURCE" "$TARGET"
-    
-    echo "$key configuwation compweted! <3"
+    echo "$key configuwation compweted!"
 done
+
+echo "autistic config applied <3"
