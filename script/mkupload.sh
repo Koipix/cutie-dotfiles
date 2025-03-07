c062e1b46676b75cc2a18a826b3f829102a0c04d
koipix <koi.devt@gmail.com>
2025-03-08 07:06:12 +0800

    Upload Script

---
+++ script/mkupload.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+DOTFILES_DIR=~/dotfiles
+CONFIG_DIR=~/.config
+LOCAL_DIR=~/.local
+BACKUP_DIR=~/dotfiles_backup
+
+mkdir -p "$BACKUP_DIR"
+mkdir -p "$DOTFILES_DIR"
+
+# awway of dotfiles; feew fwee to add youw dotfiles hewe with the wight souwce path
+declare -A DOTFILES
+
+DOTFILES=(
+    [neofetch]="$CONFIG_DIR/neofetch"
+    [Kvantum]="$CONFIG_DIR/Kvantum"
+    [konsole]="$LOCAL_DIR/share/konsole"
+)
+
+for key in "${!DOTFILES[@]}"; do
+    TARGET="${DOTFILES[$key]}"
+    SOURCE="$DOTFILES_DIR/$key"
+
+    # check if dotfile alweady exist othewise cweate the folder
+    mkdir -p "$(dirname "$SOURCE")"
+
+    if [ -L "$TARGET" ]; then
+        echo "$key already exist"
+        continue
+    fi
+
+    if [ -e "$TARGET" ] && [ ! -L "$TARGET" ]; then
+        mv "$TARGET" "$SOURCE"/..
+        echo "Moved $key to $SOURCE"
+    fi
+
+    # cweate symwink because they were meant to be together <3
+    ln -s "$SOURCE" "$TARGET"
+    echo "$key configuwation compweted!"
+done
+
+echo "autistic config uploaded <3"
